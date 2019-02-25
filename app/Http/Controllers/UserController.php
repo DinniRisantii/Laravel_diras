c4aedf774fd387b264775bb0eb43279841d8f191
dinnirisanti <dinnirisanti14@gmail.com>
2019-02-26 03:36:45 +0700

    Episode-19

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,8 +81,17 @@ class UserController extends Controller
             return redirect()->route('admin.user')->with('result','update');
         } else {
             return back()->with('result','fail');
-        }
-        
-                
+        }                 
     }
+
+   public function delete(Request $req)
+   {
+       $result = User::find($req->id);
+
+       if ( $result->delete() ) {
+           return back()->with('result','delete');
+       } else {
+        return back()->with('result','fail-delete');
+       }
+   }
 } 
